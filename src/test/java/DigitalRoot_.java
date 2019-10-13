df1c3449f8956b3b3b5a14578d41c813c235fbce
ismael094 <ismaelpitufo@gmail.com>
2019-10-13 10:55:42 +0100

    Seven test

--- src/test/java/DigitalRoot_.java
+++ src/test/java/DigitalRoot_.java
@@ -2,6 +2,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import static java.lang.Integer.parseInt;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.iterate;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(org.junit.runners.Parameterized.class)
@@ -22,6 +25,7 @@ public class DigitalRoot_ {
                 {0,0},
                 {8, 8},
                 {10,1},
+                {11,2},
                 {20,2}
         };
     }
@@ -34,8 +38,23 @@ public class DigitalRoot_ {
     private int sumOfDigits(int number) {
         if (number <= -1)
             return 0;
-        else if (number >= 10)
-            return number/10;
+        else if (number >= 10) {
+            String digits = Integer.toString(number);
+            int[] digitBreaker = getDigitArray(digits);
+            int res = 0;
+            for (int item : digitBreaker) {
+                res += item;
+            }
+            return res;
+        }
         else return number;
+    }
+
+    private int[] getDigitArray(String digits) {
+        return iterate(0, l->l+1)
+                .limit(digits.length())
+                .boxed()
+                .mapToInt( i -> parseInt(digits.substring(i,i+1)))
+                .toArray();
     }
 }
