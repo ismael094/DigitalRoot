2aa3ae897652b7a1619f8faa5f161d266cc18454
ismael094 <ismaelpitufo@gmail.com>
2019-10-13 10:25:27 +0100

    Second and third test

--- src/test/java/DigitalRoot_.java
+++ src/test/java/DigitalRoot_.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(org.junit.runners.Parameterized.class)
 public class DigitalRoot_ {
+    private final int number;
+    private final int value;
+
+    public DigitalRoot_(int number, int value) {
+        this.number = number;
+        this.value = value;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] cases() {
+        return new Object[][] {
+                {-1,0},
+                {8, 8},
+                {0,0}
+        };
+    }
+
+    @Test
+    public void execute() {
+        assertThat(sumOfDigits(number)).isEqualTo(value);
+    }
+
+    private int sumOfDigits(int number) {
+        return number == -1 ? 0 : number;
+    }
 }
